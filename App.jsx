07f24678734ab51a8f05b77e6e50262b73da14a5
sarahkirby <sk.irby@icloud.com>
2016-03-22 11:12:38 +1300

    Added Tracker React package. Pulled resolutions from collection (db).

--- App.jsx
+++ App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
 // creating database - resoltuions
 Resolutions = new Mongo.Collection("resolutions");
 
-export default class App extends React.Component {
+export default class App extends TrackerReact(React.Component) {
+
+	resolutions() {
+		// .find() returns all reasolutions in collection(db). fetch returns an object
+		return Resolutions.find().fetch();
+	}
 	addResolution(event) {
 		// stop page from reloading
 		event.preventDefault();
@@ -18,12 +24,14 @@ export default class App extends React.Component {
 
 	}
 	render() {
+		let res = this.resolutions();
+		
+		console.log(this.resolutions());
 		return (
 			<div>
 				<h1>My Resolutions</h1>
 				<form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
 					<input type="text" ref="resolution" placeholder="Finish React Meteor Series" />
-
 				</form>
 			</div>
 		)
