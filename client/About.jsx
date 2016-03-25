3be6868c17324614bc0ff0552e275048867c4dfa
sarahkirby <sk.irby@icloud.com>
2016-03-25 18:36:31 +1300

    Animated elements and page contets using the ReactCSSTransitionGroup package with some css.

--- client/About.jsx
+++ client/About.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class About extends Component {
 
@@ -8,11 +9,18 @@ export default class About extends Component {
 	// setting a meteor session variable on click
 	render() {
 		return (
-			<div>
+			<ReactCSSTransitionGroup 
+				component="div"
+				transitionName="route"
+				transitionEnterTimeout={600}
+				transitionAppearTimeout={600}
+				transitionLeaveTimeout={400}
+				transitionAppear={true}
+				>
 				<h1>About Us</h1>
-				<p>filler</p>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 				<button onClick={this.setVar}>Sign Up</button>
-			</div>
+			</ReactCSSTransitionGroup>
 		)
 	}
 }
--- client/resolutions/ResolutionsWrapper.jsx
+++ client/resolutions/ResolutionsWrapper.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import ResolutionsForm from './ResolutionsForm.jsx';
 import ResolutionSingle from './ResolutionSingle.jsx';
@@ -31,15 +32,28 @@ export default class ResolutionsWrapper extends TrackerReact(React.Component) {
 	// grabbing the session variable test from about.
 	render() {
 		return (
-			<div>
+			<ReactCSSTransitionGroup 
+			component="div"
+			transitionName="route"
+			transitionEnterTimeout={600}
+			transitionAppearTimeout={600}
+			transitionLeaveTimeout={400}
+			transitionAppear={true}
+			>
 				<h1>My Resolutions - {Session.get('test')}</h1>
 				<ResolutionsForm />
-				<ul className="resolutions">
+				<ReactCSSTransitionGroup 
+					component="ul"
+					className="resolutions"
+					transitionName="resolutionLoad"
+					transitionEnterTimeout={600}
+					transitionLeaveTimeout={400}
+					>
 					{this.resolutions().map((resolution)=>{
 						return <ResolutionSingle key={resolution._id} resolution={resolution} />
 					})}
-				</ul>
-			</div>
+					</ReactCSSTransitionGroup>
+			</ReactCSSTransitionGroup>
 		)
 	}
 }
